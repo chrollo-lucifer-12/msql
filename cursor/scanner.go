79a7f376f453381409922c6a0fbad50eeb5da39a
chrollo-lucifer-12 <sahilpython@gmail.com>
2026-08-03 16:39:21 +0000

    add: scanner

---
+++ cursor/scanner.go
@@ -0,0 +1,51 @@
+package cursor
+
+import (
+	"fmt"
+
+	"github.com/msql/pager"
+)
+
+type Scanner struct {
+	pager *pager.Pager
+	page  uint
+	cell  uint
+}
+
+func NewScanner(pager *pager.Pager, page uint) *Scanner {
+	return &Scanner{
+		pager: pager,
+		page:  page,
+		cell:  0,
+	}
+}
+
+func (s *Scanner) NextRecord() (*Cursor, error) {
+	page, err := s.pager.ReadPage(s.page)
+	if err != nil {
+		return nil, err
+	}
+
+	leaf, ok := page.(*pager.TableLeafPage)
+	if !ok {
+		return nil, fmt.Errorf("wrong page type")
+	}
+
+	cell := leaf.Cells[s.cell]
+	header, err := parseRecordHeader(cell.Payload)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Cursor{
+		header:    header,
+		pager:     s.pager,
+		pageIndex: s.page,
+		pageCell:  s.cell,
+	}
+
+	s.cell++
+
+	return c, nil
+
+}
